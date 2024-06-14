74764b722cfec1e7e41b82a221f9ba9fde4093ee
Alexey Evmenkov <a.evmenkov@dreamteam-apps.com>
2024-06-14 21:25:34 +0300

    HomeWork5

---
+++ Lesson 5_EnumsOptionals/MyPlayground.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+enum Seasons {
+    case winter
+    case spring
+    case summer
+    case autumn
+    case error
+    
+    func season(month: Int) -> Seasons {
+        switch month {
+        case 1,2,12:
+            return.winter
+        case 3...5:
+            return.spring
+        case 6...8:
+            return.summer
+        case 9...11:
+            return.autumn
+        default:
+            return.error
+        }
+    }
+}
+
+
+func variadicString(_ strings: String?...) -> Int {
+    var counter = 0
+    var stringNotNil = ""
+    
+    for string in strings {
+        if let notNilString = string {
+            stringNotNil = stringNotNil + notNilString
+        } else {
+            counter+=1
+        }
+    }
+    print(stringNotNil)
+    return counter
+}
