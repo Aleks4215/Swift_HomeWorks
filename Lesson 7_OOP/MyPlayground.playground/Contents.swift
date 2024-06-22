710f7e1cba062a0fca4e700a8a592a262397940a
Alexey Evmenkov <a.evmenkov@dreamteam-apps.com>
2024-06-22 19:15:43 +0300

    hw7

---
+++ Lesson 7_OOP/MyPlayground.playground/Contents.swift
@@ -0,0 +1,75 @@
+import UIKit
+
+class Person {
+    let name: String
+    let surname: String
+    var age: Int
+    
+    init(name: String, surname: String, age: Int) {
+        self.name = name
+        self.surname = surname
+        self.age = age
+    }
+}
+
+class Director: Person {
+    private let experience: String
+    private let rating: Double
+    
+    init(experience: String, rating: Double, name: String, surname: String, age: Int) {
+        self.experience = experience
+        self.rating = rating
+        super.init(name: name, surname: surname, age: age)
+    }
+}
+
+class Pupil: Person {
+    private var classNumber: Int
+    private var schoolAssessment: (subject: String, assessment: Int)
+    
+    init(classNumber: Int, schoolAssessment: (subject: String, assessment: Int), name: String, surname: String, age: Int) {
+        self.classNumber = classNumber
+        self.schoolAssessment = schoolAssessment
+        super.init(name: name, surname: surname, age: age)
+    }
+    
+    func aboutPupil() {
+        print("Фамилия \(surname) Имя \(name)(класс \(classNumber)) предмет \(schoolAssessment.subject) : оценка \(schoolAssessment.assessment)")
+    }
+}
+
+
+class Adress {
+    
+    var x: String
+    var y: String
+    var streetName: String
+    
+    init(x: String, y: String, streetName: String) {
+        self.x = x
+        self.y = y
+        self.streetName = streetName
+    }
+}
+
+
+struct School {
+    var students: [Pupil]
+    var titile: String
+    
+    let director: Director
+    let adress: Adress
+    
+    func aboutSchool( ) {
+        print("Школа \(titile), директор \(director.name) \(director.surname), ученики \(students.count), x \(adress.x), y \(adress.y), название улицы \(adress.streetName)")
+    }
+    
+    
+}
+
+let pupil = Pupil(classNumber: 1, schoolAssessment: ("Информатика", 4), name: "Василий", surname: "Пупкин", age: 6)
+let director = Director(experience: "10 лет", rating: 10, name: "Григорий", surname: "Васильев", age: 44)
+let adress = Adress(x: "123", y: "12", streetName: "Улица Пушкина")
+let school = School(students: [pupil], titile: "1", director: director, adress: adress)
+pupil.aboutPupil()
+school.aboutSchool()
