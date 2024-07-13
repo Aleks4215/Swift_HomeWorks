5a22598f2f877849c3b71639c4f0eadeca7539e8
Alexey Evmenkov <a.evmenkov@dreamteam-apps.com>
2024-07-13 22:08:48 +0300

    initial

--- Lesson 12_Delegates&Callbacks/hw12/hw12/EditViewController.swift
+++ Lesson 12_Delegates&Callbacks/hw12/hw12/EditViewController.swift
@@ -8,11 +8,57 @@
 import UIKit
 
 class EditViewController: UINavigationController {
+    
+    let firstName: UILabel = {
+        let label = UILabel()
+        label.text = "First name"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+   let firstNameTextField: UITextField = {
+        let textField = UITextField()
+       textField.placeholder = "Передача данных"
+       textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    let changeName: UIButton = {
+       let button = UIButton()
+        button.backgroundColor = .systemBlue
+        button.setImage(UIImage(systemName: "more"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+       return button
+    }()
+    
+    let saveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Сохранить", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        view.addSubview(firstName)
+        view.addSubview(firstNameTextField)
+        view.addSubview(changeName)
+        
+        firstName.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        firstName.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+        firstName.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+        
+        firstNameTextField.topAnchor.constraint(equalTo: firstName.bottomAnchor, constant: 10).isActive = true
+        firstNameTextField.leadingAnchor.constraint(equalTo: firstName.leadingAnchor).isActive = true
+        
+        changeName.leadingAnchor.constraint(equalTo: firstNameTextField.trailingAnchor, constant: 10).isActive = true
+        changeName.centerYAnchor.constraint(equalTo: firstNameTextField.centerYAnchor).isActive = true
+        changeName.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -5).isActive = true
     }
     
 
