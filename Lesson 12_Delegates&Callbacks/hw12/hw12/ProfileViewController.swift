3b99b4216868b3273f391da2514b6f62e9e28677
Alexey Evmenkov <a.evmenkov@dreamteam-apps.com>
2024-07-13 21:49:04 +0300

    hw12 initial

---
+++ Lesson 12_Delegates&Callbacks/hw12/hw12/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw12
+//
+//  Created by Алексей on 11.07.24.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
